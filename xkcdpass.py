f7eebcb0e5dcaa9fae419d8bc577c16093418edb
Army-R <80074550+Army-R@users.noreply.github.com>
2021-11-04 21:22:52 -0600

    Excersice with xkcdpass library

---
+++ xkcdpass.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 19:31:31 2021
+
+@author: Army-R
+"""
+# Passphrase generator
+# https://xkcd.com/936/
+
+from xkcdpass import xkcd_password as xp
+
+words = xp.generate_wordlist(min_length=5, max_length=8)
+
+print(xp.generate_xkcdpassword(words, numwords=4))
